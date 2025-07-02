d8197cc054518342b44f313afc8c7bc79382368d
nimonroy_meli <nimonroy@co000htw66hmhf3.ml.com>
2025-07-02 15:57:32 -0500

    requerimiento 3: mejora la peticion update, verifica la estructura fuera de nils.

--- pkg/models/section_model.go
+++ pkg/models/section_model.go
@@ -5,7 +5,7 @@ type Section struct {
 	// ID is the unique identifier of the section
 	ID int `json:"id" validate:"required"`
 	// SectionNumber is the number of the section
-	SectionNumber int `json:"sectionNumber" validate:"required,gte=0"`
+	SectionNumber int `json:"sectionNumber" validate:"required,gt=0"`
 	// CurrentTemperature is the current temperature of the section
 	CurrentTemperature float64 `json:"currentTemperature" validate:"required,gtefield=MinimumTemperature"`
 	// MinimumTemperature is the minimum temperature that can be maintained in the section
@@ -20,4 +20,22 @@ type Section struct {
 	WarehouseID int `json:"warehouseID" validate:"required,gte=1"`
 	// ProductTypeID is the unique identifier of the type of product stored in the section
 	ProductTypeID int `json:"productTypeID" validate:"required,gte=1"`
+}
+
+type SectionPatch struct {
+	SectionNumber *int
+	// CurrentTemperature is the current temperature of the section
+	CurrentTemperature *float64 `json:"currentTemperature"`
+	// MinimumTemperature is the minimum temperature that can be maintained in the section
+	MinimumTemperature *float64 `json:"minimumTemperature" `
+	// CurrentCapacity is the current capacity of the section
+	CurrentCapacity *int `json:"CurrentCapacity"`
+	// MinimumCapacity is the minimum capacity of the section
+	MinimumCapacity *int `json:"minimumCapacity"`
+	// MaximumCapacity is the maximum capacity of the section
+	MaximumCapacity *int `json:"maximumCapacity"`
+	// WarehouseID is the unique identifier of the warehouse to which the section belongs
+	WarehouseID *int `json:"warehouseID"`
+	// ProductTypeID is the unique identifier of the type of product stored in the section
+	ProductTypeID *int `json:"productTypeID"`
 }
