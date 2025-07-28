87f7e71eca7a49dceee8e0c83dd239157e649b38
Luis Carlos Medina <luis.medinamoreno@mercadolibre.com.co>
2025-07-28 16:10:04 -0500

    feat testing repository inbound and employee

--- tests/employee_repo_mock.go
+++ tests/employee_repo_mock.go
@@ -1,55 +1,22 @@
 package tests
 
-import (
-	"database/sql/driver"
-	mod "github.com/smartineztri_meli/W17-G2-Bootcamp/pkg/models"
+import "database/sql/driver"
+
+var EmployeeTableStruct = []string{"id", "id_card_number", "first_name", "last_name", "wareHouse_id"}
+
+var EmployeeDataValuesSelect = [][]driver.Value{
+	{1, "123", "John", "Doe", 10},
+	{2, "456", "Jane", "Smith", 20},
+}
+
+var EmployeeDataValuesSelectByID = []driver.Value{
+	1, "123", "John", "Doe", 10,
+}
+
+var (
+	EmployeeSelectExpectedQuery      = "SELECT id,id_card_number,first_name,last_name, wareHouse_id FROM employees"
+	EmployeeSelectWhereExpectedQuery = "SELECT id,id_card_number,first_name,last_name, wareHouse_id FROM employees WHERE id = ?"
+	EmployeeInsertExpectedQuery      = "INSERT INTO employees (id_card_number,first_name,last_name, wareHouse_id ) VALUES (?, ?,?,?)"
+	EmployeeUpdateExpectedQuery      = "UPDATE employees SET id_card_number = ?, first_name = ?, last_name = ?, wareHouse_id = ? WHERE id = ?"
+	EmployeeDeleteExpectedQuery      = "DELETE FROM employees WHERE id = ?"
 )
-
-var SectionTableStruct = []string{`id`, `section_number`, `current_temperature`, `minimum_temperature`, `current_capacity`, `minimum_capacity`, `maximum_capacity`, `warehouse_id`, `product_type_id`}
-
-var SectionDataValuesSelect = [][]driver.Value{
-	{
-		1, 1, 0, -5, 50, 20, 100, 1, 1,
-	},
-	{
-		2, 2, -2, -6, 60, 30, 110, 2, 2,
-	},
-}
-
-var SectionDataValuesSelectByID = []driver.Value{
-	2, 2, -2, -6, 60, 30, 110, 2, 2,
-}
-
-var SectionSelectExpectedQuery = "SELECT `id`, `section_number`,`current_temperature`,`minimum_temperature`,`current_capacity`, `minimum_capacity`,`maximum_capacity`,`warehouse_id`,`product_type_id` FROM `sections`"
-
-var SectionSelectWhereExpectedQuery = "SELECT `id`, `section_number`,`current_temperature`,`minimum_temperature`,`current_capacity`, `minimum_capacity`,`maximum_capacity`,`warehouse_id`,`product_type_id`  FROM `sections` WHERE `id`=?"
-
-type MockSectionService struct {
-	MockFindAll        func() ([]mod.Section, error)
-	MockFindById       func(id int) (mod.Section, error)
-	MockCreate         func(section *mod.Section) error
-	MockDelete         func(id int) error
-	MockUpdate         func(id int, fields map[string]interface{}) (*mod.Section, error)
-	MockReportProducts func(ids []int) ([]mod.ReportProductsResponse, error)
-}
-
-func (m *MockSectionService) FindAll() ([]mod.Section, error) {
-	return m.MockFindAll()
-}
-
-func (m *MockSectionService) FindByID(id int) (mod.Section, error) {
-	return m.MockFindById(id)
-}
-func (m *MockSectionService) Delete(id int) error {
-	return m.MockDelete(id)
-}
-func (m *MockSectionService) Update(id int, fields map[string]interface{}) (*mod.Section, error) {
-	return m.MockUpdate(id, fields)
-}
-func (m *MockSectionService) ReportProducts(ids []int) ([]mod.ReportProductsResponse, error) {
-	return m.MockReportProducts(ids)
-}
-
-func (m *MockSectionService) Save(section *mod.Section) error {
-	return m.MockCreate(section)
-}
